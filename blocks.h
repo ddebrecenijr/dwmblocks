da6c8b365427f8e5148680aacc2e43cbe183902d
David Debreceni Jr <davidjr@debreceni.net>
2020-07-22 21:49:17 -0500

    Create battery script

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",		"battery.sh",		5,			3},
 	{"",		"datetime.sh",		60,			0},
 };
 
