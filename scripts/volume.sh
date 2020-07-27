4b927094b48a541b7d3e6a622cf2ae8cba8a9ec4
David Debreceni Jr <davidjr@debreceni.net>
2020-07-26 20:22:44 -0500

    Add volume script

---
+++ scripts/volume.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+status=$(amixer sget Master | taile -n1 | sed -r "s/.*\[(.*)\]/\1/")
+vol=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
+
+off=婢
+low=
+med=墳
+high=
+mute=ﱝ
+
+if [[ $status == "off" ]]; then
+    echo " $mute MUTED "
+elif [[ $vol == 0 ]]; then
+    echo " $off $vol% "
+elif [[ $vol < 25 ]]; then
+    echo " $low $vol% "
+elif [[ $vol < 75 ]]; then
+    echo " $med $vol% "
+else
+    echo " $high $vol %"
+fi
