ca58e8533ef9d6b9ca02fe36b33da6106520d9bb
David Debreceni Jr <davidjr@debreceni.net>
2020-07-26 21:58:21 -0500

    Change my mind, don't want to see brightness

--- blocks.h
+++ blocks.h
@@ -1,7 +1,6 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"",        "brightness.sh",            0,                  11},
     {"",        "volume.sh",                0,                  10},
 	{"",		"battery.sh",		        5,			        3},
 	{"",		"datetime.sh",		        60,			        0},
